bc20bf57c9c05b0062457907b441d5f5d93cd86c
sagarchavan07 <chavansagar96044@gmail.com>
2022-09-20 23:47:52 +0530

    used two way binding and added Regex validation on userName

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ export class AppComponent {
   title = 'HelloWorld';
   imgUrl = "../assets/BridgeLabz_Logo.svg";
   blUrl = "https://www.bridgelabz.com/";
+  userName = "";
+  nameError = "";
 
   ngOnInit():void{
     this.title = "Hello from Bridgelabz";
@@ -17,5 +19,15 @@ export class AppComponent {
   onClick($event: any){
     console.log("logo is clicked", $event);
     window.open(this.blUrl, "_blank")
+  }
+  onInput($event: any){
+    console.log("verifying input", $event.data)
+    const nameRegex = RegExp("^[A-Z]{1}[\\w]{2,}$");
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    } else {
+      this.nameError = "Invalid Name";
+    }
   }
 }
