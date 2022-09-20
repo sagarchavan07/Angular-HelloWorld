de6e93c7d202c62578131e2164ccf2d84a84862e
sagarchavan07 <chavansagar96044@gmail.com>
2022-09-20 21:00:35 +0530

    added logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld';
+  imgUrl = "../assets/BridgeLabz_Logo.svg"
 
   ngOnInit():void{
     this.title = "Hello from Bridgelabz";
